eb59163a541678985ece314a3d3c0aedb118f3bb
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-09-28 00:41:28 +0530

    added fallback functionality
    And fixed potential reentrency attack in withdraw

--- WETH.sol
+++ WETH.sol
@@ -1,16 +1,26 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
-    constructor() ERC20("Wrapped ETH", "WETH") public {}
+    event Deposit (address indexed account, uint amount);
+    event Withdraw (address indexed account, uint amount);
     
-    function mint() external payable {
-        _mint(msg.sender, msg.value);
+    constructor() ERC20("Wrapped ETH", "WETH") {}
+    
+    fallback() external payable {
+       mint();
     }
     
-    function burn(uint amount) external {
-        msg.sender.transfer(amount);
+    function mint() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit (msg.sender, msg.value);
+    }
+    
+    function withdraw(uint amount) external {
         _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount);
     }
 }
